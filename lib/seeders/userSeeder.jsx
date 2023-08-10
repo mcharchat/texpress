89c147bcf68304efc424b33f1870a724917cb702
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-08-10 01:02:09 -0300

    roles Dict

--- lib/seeders/userSeeder.jsx
+++ lib/seeders/userSeeder.jsx
@@ -4,13 +4,22 @@ import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 
 export const userSeeder = async (number) => {
+
+	const rolesDict = {
+		0: "superadmin",
+		1: "administrator",
+		2: "editor",
+		3: "author",
+		4: "contributor",
+		5: "subscriber",
+	};
 	const users = [];
 	for (let i = 0; i < number; i++) {
 		users.push({
 			name: faker.person.firstName() + " " + faker.person.lastName(),
 			email: faker.internet.email(),
 			password: bcrypt.hashSync(process.env.TEST_USER_PASSWORD, 10),
-			role: Math.floor(Math.random() * [0, 1, 2].length),
+			role: Math.floor(Math.random() * [0, 1, 2, 3, 4, 5].length),
 			img: faker.image.url(),
 		});
 	}
