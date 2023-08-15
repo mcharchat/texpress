53963f57a9f75e5cd6740ac1b59345062e9b6624
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-08-15 16:23:31 -0300

    updated mailservice for gmail

--- lib/utils/mailService.jsx
+++ lib/utils/mailService.jsx
@@ -1,35 +1,37 @@
-import nodemailer from "nodemailer";
-
-// async..await is not allowed in global scope, must use a wrapper
 export default async function sendMail(to, subject, html) {
+	let nodemailer = require("nodemailer");
+	// create reusable transporter object using the default SMTP transport
+	let transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: process.env.MAIL_USERNAME,
+			pass: process.env.MAIL_PASSWORD,
+		},
+		tls: {
+			rejectUnauthorized: false,
+		},
+	});
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        secure: true,
-        auth: {
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD,
-        },
-        tls: {
-            // do not fail on invalid certs
-            rejectUnauthorized: false,
-        },
-    })
+	// send mail with defined transport object
+	let info = await transporter.sendMail(
+		{
+			from: `"${process.env.MAIL_FROM_NAME}" <${process.env.MAIL_FROM_ADDRESS}>`, // sender address
+			to: to.join(", "), // list of receivers
+			subject: subject, // Subject line
+			html: html, // html body
+		},
+		function (error, info) {
+			if (error) {
+				throw new Error(error);
+			} else {
+				console.log("Email Sent");
+				return true;
+			}
+		}
+	);
+	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: `"${process.env.MAIL_FROM_NAME}" <${process.env.MAIL_FROM_ADDRESS}>`, // sender address
-        to: to.join(', '), // list of receivers
-        subject: subject, // Subject line
-        html: html, // html body
-    })
-
-    //console.log("Message sent: %s", info.messageId)
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+	// Preview only available when sending through an Ethereal account
+	//console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+	// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
